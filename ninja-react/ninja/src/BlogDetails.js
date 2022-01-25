37e2686b89992ee28aca131e81a6f0ee38707b64
Tommy <86784639+tommy88520@users.noreply.github.com>
2022-01-25 14:30:04 +0800

    test

--- ninja-react/ninja/src/BlogDetails.js
+++ ninja-react/ninja/src/BlogDetails.js
@@ -1,7 +1,13 @@
 import { useParams } from 'react-router'
+import useFetch from './useFetch'
 
 const BlogDetails = () => {
   const { id } = useParams()
+  const {
+    data: blog,
+    error,
+    isPending,
+  } = useFetch('http://localhost/8000/blogs' + id)
   return (
     <>
       <div className="blog-details">
